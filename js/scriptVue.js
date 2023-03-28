7cd095ee2422946d0d37d2008c158a89d11adfea
totarochristian <totaro.christian.18@gmail.com>
2023-03-28 17:12:01 +0200

    Move functions in methods of vue app

--- js/scriptVue.js
+++ js/scriptVue.js
@@ -51,65 +51,68 @@ const app = createApp({
           img: "panchina.jpg",
           title: "Parco in Via delle Cave"
         }
-      ],
-      UpdatePreviewSlideShowed() {
-        document
-          .getElementById("previewImg_" + this.sliderPrevIndex)
-          .classList.remove("activeImage");
-        document
-          .getElementById("previewImg_" + this.sliderCurrentIndex)
-          .classList.add("activeImage");
-      },
-      UpdateSlideShowed() {
-        document
-          .getElementById("sliderImg")
-          .setAttribute(
-            "src",
-            "./assets/img/" + this.sliderImages[this.sliderCurrentIndex].img
-          );
-        document.getElementById("sliderTitle").innerText =
-        this.sliderImages[this.sliderCurrentIndex].title;
-        this.UpdatePreviewSlideShowed();
-      },
-      PrevSlide() {
-        this.sliderPrevIndex = this.sliderCurrentIndex;
-        if (this.sliderCurrentIndex > 0) this.sliderCurrentIndex--;
-        else this.sliderCurrentIndex = this.sliderImages.length - 1;
-        this.UpdateSlideShowed();
-      },
-      NextSlide() {
-        this.sliderPrevIndex = this.sliderCurrentIndex;
-        if (this.sliderCurrentIndex < this.sliderImages.length - 1) this.sliderCurrentIndex++;
-        else this.sliderCurrentIndex = 0;
-        this.UpdateSlideShowed();
-      },
-      AddPreviewImages() {
-        const preview = document.getElementsByClassName("preview")[0];
-        for (let i = 0; i < this.sliderImages.length; i++) {
-          preview.innerHTML +=
-            '<div id="previewImg_' +
-            i +
-            '" class="imgCont"><img src="./assets/img/' +
-            this.sliderImages[i].img +
-            '" alt="Immagine ' +
-            i +
-            ' della preview"></div>';
-          document.getElementById("previewImg_" + i).style.height =
-            "" + 100 / this.sliderImages.length + "%";
-        }
-      },
-      MouseWheelScroll(event) {
-        const delta = Math.sign(event.deltaY);
-        if (delta == 1) NextSlide();
-        else if (delta == -1) PrevSlide();
-      },
-      StartAutoCarousel() {
-        this.intervalSlider = setInterval(this.NextSlide, 3000);
-      },
-      StopAutoCarousel() {
-        clearInterval(this.intervalSlider);
-      },
+      ]
     };
+  },
+  methods: {
+    UpdatePreviewSlideShowed() {
+      document
+        .getElementById("previewImg_" + this.sliderPrevIndex)
+        .classList.remove("activeImage");
+      document
+        .getElementById("previewImg_" + this.sliderCurrentIndex)
+        .classList.add("activeImage");
+    },
+    UpdateSlideShowed() {
+      document
+        .getElementById("sliderImg")
+        .setAttribute(
+          "src",
+          "./assets/img/" + this.sliderImages[this.sliderCurrentIndex].img
+        );
+      document.getElementById("sliderTitle").innerText =
+        this.sliderImages[this.sliderCurrentIndex].title;
+      this.UpdatePreviewSlideShowed();
+    },
+    PrevSlide() {
+      this.sliderPrevIndex = this.sliderCurrentIndex;
+      if (this.sliderCurrentIndex > 0) this.sliderCurrentIndex--;
+      else this.sliderCurrentIndex = this.sliderImages.length - 1;
+      this.UpdateSlideShowed();
+    },
+    NextSlide() {
+      this.sliderPrevIndex = this.sliderCurrentIndex;
+      if (this.sliderCurrentIndex < this.sliderImages.length - 1)
+        this.sliderCurrentIndex++;
+      else this.sliderCurrentIndex = 0;
+      this.UpdateSlideShowed();
+    },
+    AddPreviewImages() {
+      const preview = document.getElementsByClassName("preview")[0];
+      for (let i = 0; i < this.sliderImages.length; i++) {
+        preview.innerHTML +=
+          '<div id="previewImg_' +
+          i +
+          '" class="imgCont"><img src="./assets/img/' +
+          this.sliderImages[i].img +
+          '" alt="Immagine ' +
+          i +
+          ' della preview"></div>';
+        document.getElementById("previewImg_" + i).style.height =
+          "" + 100 / this.sliderImages.length + "%";
+      }
+    },
+    MouseWheelScroll(event) {
+      const delta = Math.sign(event.deltaY);
+      if (delta == 1) NextSlide();
+      else if (delta == -1) PrevSlide();
+    },
+    StartAutoCarousel() {
+      this.intervalSlider = setInterval(this.NextSlide, 3000);
+    },
+    StopAutoCarousel() {
+      clearInterval(this.intervalSlider);
+    }
   },
   mounted() {
     this.AddPreviewImages();
