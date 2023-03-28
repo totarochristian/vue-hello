fdec249c007cd8948df3804317ceab465ec9d57d
totarochristian <totaro.christian.18@gmail.com>
2023-03-28 16:09:46 +0200

    AddimageName field in app data

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return{
-            title: 'Vue hello'
+            title: 'Vue hello',
+            imageName: 'bonfire.jpg'
         }
     }
 }).mount('#app');
